d3510d5e5887bad3a67501d0a799849c4367a9cd
Tarik <106037000+Tarik2508@users.noreply.github.com>
2024-02-27 18:18:39 +0100

    Update savePrDetails.js

--- savePrDetails.js
+++ savePrDetails.js
@@ -4,5 +4,5 @@ const prDetails = process.env.PR_DETAILS;
 
 fileSystem.writeFile('pr_details.json', prDetails, (err) => {
   if (err) throw err;
-  console.log('PR details have been saved!');
+  console.log('PR details have been saved');
 });
